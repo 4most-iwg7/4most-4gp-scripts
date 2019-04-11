70dfd4f23aa1eb7ff878d49d14677ca0581ab604
Gregor Traven <travegre@aurora1.int.lunarc>
2019-04-11 12:08:14 +0200

    change structure of passing parameters to Payne

--- src/scripts/test_payne/examples/test.sh
+++ src/scripts/test_payne/examples/test.sh
@@ -14,3 +14,24 @@ mkdir -p ../../../output_data/payne
 
 # ----------------------
 
+python3 payne_test2.py --train "galah_training_sample_4fs_lrs[SNR=250]" \
+                       --test "galah_test_sample_4fs_lrs" \
+                       --reload-payne "true" \
+                       --censor "line_list_filter_2016MNRAS.461.2174R.txt" \
+                       --description "4MOST LRS (censored) - Payne 10 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
+                       --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
+                       --assume-scaled-solar \
+                       --num-training-workers 40 \
+                       --num-testing-workers 40 \
+                       --output-file "../../../output_data/payne/s2_payne_galah_censored_lrs_10label_10neuron" \
+                       --neuron-count "10"
+
+#python3 payne_test2.py --train "galah_training_sample_4fs_lrs[SNR=250]" \
+#                       --test "galah_test_sample_4fs_lrs" \
+#                       --censor "line_list_filter_2016MNRAS.461.2174R.txt" \
+#                       --description "4MOST LRS (censored) - Payne 10 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
+#                       --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
+#                       --assume-scaled-solar \
+#                       --output-file "../../../output_data/payne/payne_galah_censored_lrs_10label" \
+#                       --neuron-count "5" 
+
