ee1de8263113cad07a3c1420353de8ae4b55cd58
Gregor Traven <travegre@aurora1.int.lunarc>
2019-06-27 12:06:44 +0200

    some final Payne corrections before summer, lunarc scripts

--- src/scripts/test_payne/examples/test.sh
+++ src/scripts/test_payne/examples/test.sh
@@ -15,17 +15,31 @@ mkdir -p ../../../output_data/payne
 # ----------------------
 export OMP_NUM_THREADS=1
 
-python3 payne_test2.py --train "galah_training_sample_4fs_lrs[SNR=250]" \
-                       --test "galah_test_sample_4fs_lrs" \
+# python3 payne_test2.py --train "galah_training_sample_4fs_lrs[SNR=250]" \
+#                        --test "galah_test_sample_4fs_lrs" \
+#                        --reload-payne "false" \
+#                        --censor "line_list_filter_2016MNRAS.461.2174R.txt" \
+#                        --description "4MOST LRS (censored) - Payne 3 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
+#                        --labels "Teff,logg,[Fe/H]" \
+#                        --assume-scaled-solar \
+#                        --num-training-workers 40 \
+#                        --num-testing-workers 40 \
+#                        --output-file "../../../output_data/payne/payne_galah_censored_lrs_3label_10neuron" \
+#                        --neuron-count "10"
+
+
+
+python3 payne_test2.py --train "galah_training_sample_4fs_hrs[SNR=250]" \
+                       --test "galah_test_sample_4fs_hrs" \
                        --reload-payne "true" \
                        --censor "line_list_filter_2016MNRAS.461.2174R.txt" \
-                       --description "4MOST LRS (censored) - Payne 10 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
+                       --description "4MOST HRS (censored) - Payne 10 labels - Train on 0.25 GALAH. Test on 0.75 GALAH." \
                        --labels "Teff,logg,[Fe/H],[Ca/H],[Mg/H],[Ti/H],[Si/H],[Na/H],[Ni/H],[Cr/H]" \
                        --assume-scaled-solar \
                        --num-training-workers 40 \
                        --num-testing-workers 40 \
-                       --output-file "../../../output_data/payne/s2_payne_galah_censored_lrs_10label_10neuron" \
-                       --neuron-count "10"
+                       --output-file "../../../output_data/payne/payne_galah_censored_hrs_10label_20neuron" \
+                       --neuron-count "20"
 
 
 
